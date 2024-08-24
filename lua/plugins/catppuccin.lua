e1292717e40c56cf053bcfa9b4036e23b84997e7
polizz <polizz@nowhere>
2024-08-24 01:02:32 -0500

    Fix catpuccin latte neotree bg color contrast

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,5 +1,23 @@
+
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  priority = 1000
+  priority = 1000,
+  -- opts = {
+  --     transparent_background = true,
+  --     integrations = {
+  --       telescope = true,
+  --       harpoon = true
+  --   }},
+  config = function()
+    require("catppuccin").setup({
+      highlight_overrides = {
+        latte = function(latte)
+          return {
+            NeoTreeCursorLine = {  bg = latte.surface0  },
+          }
+        end
+        }
+      })
+    end
 }
