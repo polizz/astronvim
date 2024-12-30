5d6bdf8d63ca20b0f430ab6786d82cc13061ce30
polizz <polizz@nowhere>
2024-12-30 11:40:37 -0600

    Add avante, conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/conform.nvim",
+  -- opts = {},
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- You can customize some of the format options for the filetype (:help conform.format)
+        markdown = { "markdownfmt" },
+        -- Conform will run the first available formatter
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    })
+    end
+}
