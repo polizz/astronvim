425f5e01d23f87aa6d116a87d9f0be4a5951a79b
polizz <polizz@nowhere>
2025-06-03 17:35:36 -0500

    Add default treesitter parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,6 +10,12 @@ return {
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
       "vim",
+      "json",
+      "rust",
+      "markdown",
+      "markdown_inline",
+      "hurl",
+      "yaml"
       -- add more arguments for adding more treesitter parsers
     })
   end,
