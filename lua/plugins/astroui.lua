66fa67e837f665423a5e5ad128834ac6465aff60
polizz <polizz@nowhere>
2025-11-03 13:48:10 -0600

    Change visual selection bg color

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -11,8 +11,9 @@ return {
       --   LineNr = { fg = "#555555" },
       -- },
       init = {
-        LineNr = { link = "Conceal" },
-        -- LineNr = { fg = "#555555" },
+        Visual = { bg = "#444444" },
+        linenr = { link = "conceal" },
+        -- linenr = { fg = "#555555" },
         -- SnacksPickerDir = { link = "Comment" },          -- or set a specific fg
         SnacksPickerDir = { link = "Conceal" },          -- or set a specific fg
         -- SnacksPickerDir = { link = "Directory" },          -- or set a specific fg
